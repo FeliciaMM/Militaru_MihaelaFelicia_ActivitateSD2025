e9627bf69649c47b62f6a31e45b4e5ff95d2d41f
Felicia <feliciamihaela2@gmail.com>
2025-03-16 22:11:01 +0200

    Vectori ex 1+2

---
+++ seminarSDD/Task suplimentar4.c
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+
+
+struct Film {
+	char* titlu;
+	int nrActori;
+	float durata;
+	int nrRecenzii;
+	float* note;
+};
+
+struct Film initializare(char* titlu, int nrActori, float durata, int nrRecenzii, float* note) {
+	struct Film f;
+	f.titlu = (char*)malloc((strlen(titlu) + 1) * sizeof(char));
+	strcpy(f.titlu, titlu);
+	f.nrActori = nrActori;
+	f.durata = durata;
+	f.nrRecenzii = nrRecenzii;
+	f.note = (float*)malloc(nrRecenzii * sizeof(float));
+	for (int i = 0; i < nrRecenzii; i++) {
+		f.note[i] = note[i];
+	}
+	return f;
+}
+
+void afisareFilm(struct Film film) {
+	printf("\nTitlu: %s\n", film.titlu);
+	printf("Numar actori: %d\n", film.nrActori);
+	printf("Durata: %.2f\n", film.durata);
+	printf("Numar recenzii: %d\n", film.nrRecenzii);
+	if (film.nrRecenzii > 0) {
+		printf("Note din recenzii: ");
+		for (int i = 0; i < film.nrRecenzii; i++) {
+			printf("%.2f ", film.note[i]);
+		}
+		printf("\n");
+	}
+	else {
+		printf("Nu exista recenzii disponibile.");
+	}
+}
+
+void afisareVector(struct Film* filme, int nrFilme) {
+	for (int i = 0; i < nrFilme; i++) {
+		afisareFilm(filme[i]);
+	}
+}
+
+
+int main() {
+	struct Film* filme = NULL;
+	int nrFilme = 5;
+	filme = (struct Film*)malloc(nrFilme * sizeof(struct Film));
+	filme[0] = initializare("Titanic", 5, 180, 3, (float[]){ 8.5, 9.0, 7.5 });
+	filme[1] = initializare("Somwhere in time", 4, 120, 4, (float[]) { 7, 6.5, 8.5, 7 });
+	filme[2] = initializare("It", 7, 150, 2, (float[]) { 9.5,9 });
+	filme[3] = initializare("Spider-Man",5, 135, 3, (float[]) { 7, 6.5, 8.5 });
+	filme[4] = initializare("Morbius", 3, 95, 3, (float[]) { 5, 6.5, 4});
+
+	afisareVector(filme, nrFilme);
+}
