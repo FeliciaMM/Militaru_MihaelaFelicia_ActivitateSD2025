fe4f9be00526db2536f7430d4e9e31714f22a751
Felicia <feliciamihaela2@gmail.com>
2025-05-12 18:02:27 +0300

    corectarea numerelor seminarelor

---
+++ seminarSDD/Sablon Arbore Binar.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct StructuraMasina {
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* numeSofer;
+	unsigned char serie;
+};
+typedef struct StructuraMasina Masina;
+typedef struct Node Node;
+
+struct Node {
+	Node* l;
+	Node* r;
+	Masina info;
+};
+
+Masina citireMasinaDinFisier(FILE* file) {
+
+}
