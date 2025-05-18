e9c8be70a1eb46cb210f4b5b43184884c58b9c73
Felicia <feliciamihaela2@gmail.com>
2025-05-18 20:45:29 +0300

    finalizare aprofundare heap 2

---
+++ seminarSDD/Heap aprofundare 2.c
@@ -0,0 +1,141 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//poz*2-1 - fiu
+//poz*2-2 - fiu
+//(nrElemente-2)/2 verifica cate noduri sunt parinti
+struct Student {
+	int id;
+	char* nume;
+	int an;
+};
+
+typedef struct Student Student;
+
+struct Heap {
+	int lungime;
+	int nrElemente;
+	Student* vector; 
+};
+
+typedef struct Heap Heap;
+
+Student citireStudentDinFisier(FILE* file) {
+	char buffer[100];
+	char sep[3] = ",\n";
+	fgets(buffer, 100, file);
+	char* aux;
+	Student s;
+	aux = strtok(buffer, sep);
+	s.id = atoi(aux);
+	aux = strtok(NULL, sep);
+	s.nume = (char*)malloc(strlen(aux) + 1);
+	strcpy(s.nume, aux);
+	aux = strtok(NULL, sep);
+	s.an = atoi(aux);
+
+	return s;
+}
+
+void afiseazaStudent(Student s) {
+	printf("\nStudent: %s", s.nume);
+	printf("\nId: %d", s.id);
+	printf("\nAnul: %d", s.an);
+}
+
+Heap initializareHeap(int lungime) {
+	Heap heap;
+	heap.lungime = lungime;
+	heap.nrElemente = 0;
+	//trebuie alocata memorie pt heap cu sizeof struct-ul nostru * lungime
+	heap.vector = (Student*)malloc(sizeof(Student)*heap.lungime);
+	return heap;
+}
+
+void filtrareHeap(Heap heap,int poz) {
+	int pozFius = poz * 2 - 1;
+	int pozFiud = poz * 2 - 2;
+	//cautam pozitia maxima incepand cu nodul care ni se da
+	int pozMaxim = poz;
+	//daca fii au val mai mare, mutam poz maxima pe pozitia lor
+	if (pozFius <= heap.lungime && heap.vector[pozFius].id > heap.vector[pozMaxim].id) {
+		pozMaxim = pozFius;
+	}
+	if (pozFiud < heap.nrElemente && heap.vector[pozFiud].id > heap.vector[pozMaxim].id) {
+		pozMaxim = pozFiud;
+	}
+	if (pozMaxim < poz) {
+		Student aux = heap.vector[pozMaxim];
+		heap.vector[pozMaxim] = heap.vector[poz];
+		heap.vector[poz] = aux;
+		//verificam daca maximul mai are fii si daca trebuie mutati
+		//pozMax <= (nrElemente-2)/2 verifica daca poz este nod parinte sau frunza
+		//(7 - 2) / 2 = 2
+		//Deci nodurile de la index 0, 1, și 2 pot avea fii
+		//Nodurile 3, 4, 5, 6 sunt frunze(nu au fii)
+		if (pozMaxim <= (heap.nrElemente - 2) / 2) {
+			filtrareHeap(heap, pozMaxim);
+		}
+	}
+}
+
+Heap citireHeapDinFisier(char* numeFisier) {
+	FILE* file = fopen(numeFisier, "r");
+	Heap heap = initializareHeap(10);
+	if (file) {
+		while (!feof(file)) {
+			Student s = citireStudentDinFisier(file);
+			heap.vector[heap.nrElemente++] = s;
+		}
+		fclose(file);
+		//trecem prin toate nodurile parinte
+		for (int i = (heap.nrElemente - 2) / 2; i >= 0; i--) {
+			filtrareHeap(heap, i);
+		}
+	}
+	return heap;
+}
+
+Student extragereStudentDinHeap(Heap* heap) {
+	//interschimbare intre primul si ultimul element;
+	if (heap->nrElemente > 0) {
+		Student aux = heap->vector[0];
+		heap->vector[0] = heap->vector[heap->nrElemente - 1];
+		heap->vector[heap->nrElemente - 1] = aux;
+		heap->nrElemente--;
+		for (int i = (heap->nrElemente - 2) / 2; i >= 0; i--) {
+			filtrareHeap(*heap, i);
+		}
+		return aux;
+	}	
+}
+
+void dezalocareHeap(Heap* heap) {
+	for (int i = 0; i < heap->lungime; i++) {
+		free(heap->vector[i].nume);
+	}
+	free(heap->vector);
+	heap->vector = NULL;
+	heap->lungime = 0;
+	heap->nrElemente = 0;
+}
+
+void afiseazaHeap(Heap heap) {
+	for (int i = 0; i < heap.nrElemente; i++) {
+		printf("\n");
+		afiseazaStudent(heap.vector[i]);
+	}
+}
+
+int main() {
+	Heap heap = citireHeapDinFisier("studenti.txt");
+	afiseazaHeap(heap);
+
+	printf("--------element scos-------");
+	Student scos = extragereStudentDinHeap(&heap);
+	afiseazaStudent(scos);
+	dezalocareHeap(&heap);
+	return 0;
+}
--- seminarSDD/Seminar9-avl.c
+++ seminarSDD/Seminar9-avl.c
@@ -11,43 +11,14 @@
 //	int nrUsi;
 //	float pret;
 //	char* model;
-//	char* numeSofer;
+//	char* numeSofer; 
 //	unsigned char serie;
 //};
 //typedef struct StructuraMasina Masina;
 //
 ////creare structura pentru un nod dintr-un arbore binar de cautare
 //
-//Masina citireMasinaDinFisier(FILE* file) {
-//	char buffer[100];
-//	char sep[3] = ",\n";
-//	fgets(buffer, 100, file);
-//	char* aux;
-//	Masina m1;
-//	aux = strtok(buffer, sep);
-//	m1.id = atoi(aux);
-//	m1.nrUsi = atoi(strtok(NULL, sep));
-//	m1.pret = atof(strtok(NULL, sep));
-//	aux = strtok(NULL, sep);
-//	m1.model = malloc(strlen(aux) + 1);
-//	strcpy_s(m1.model, strlen(aux) + 1, aux);
-//
-//	aux = strtok(NULL, sep);
-//	m1.numeSofer = malloc(strlen(aux) + 1);
-//	strcpy_s(m1.numeSofer, strlen(aux) + 1, aux);
-//
-//	m1.serie = *strtok(NULL, sep);
-//	return m1;
-//}
-//
-//void afisareMasina(Masina masina) {
-//	printf("Id: %d\n", masina.id);
-//	printf("Nr. usi : %d\n", masina.nrUsi);
-//	printf("Pret: %.2f\n", masina.pret);
-//	printf("Model: %s\n", masina.model);
-//	printf("Nume sofer: %s\n", masina.numeSofer);
-//	printf("Serie: %c\n\n", masina.serie);
-//}
+////SA IMPLEMENTEZE FELICIA CITIREA SA NU MAI FIE TUTA
 //
 ////int calculeazaInaltimeArbore(/*arbore de masini*/) {
 ////	//calculeaza inaltimea arborelui care este data de 
