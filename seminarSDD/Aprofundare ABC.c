7e3b6196b85570135775f2cdde12426b3daab79e
Felicia <feliciamihaela2@gmail.com>
2025-05-25 18:07:45 +0300

    aprofundare avl + task suplimentar avl

--- seminarSDD/Aprofundare ABC.c
+++ seminarSDD/Aprofundare ABC.c
@@ -1,171 +1,172 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-struct Student {
-	int id;
-	char* nume;
-	int an;
-};
-typedef struct Student Student;
-typedef struct Node Node;
-
-struct Node {
-	Student info;
-	Node* st;
-	Node* dr;
-};
-
-Student initializareStudent(int id, const char* nume, int an) {
-	Student s;
-	s.id = id;
-	s.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
-	strcpy(s.nume, nume);
-	s.an = an;
-
-	return s;
-}
-
-Student citireStudentDinFisier(FILE* file) {
-	char buffer[100];
-	char sep[3] = ",\n";
-	fgets(buffer, 100, file);
-	char* aux;
-	Student s;
-	aux = strtok(buffer, sep);
-	s.id = atoi(aux);
-	aux = strtok(NULL, sep);
-	s.nume = (char*)malloc(strlen(aux) + 1);
-	strcpy(s.nume, aux);
-	aux = strtok(NULL, sep);
-	s.an = atoi(aux);
-
-	return s;
-}
-
-void afiseazaStudent(Student s) {
-	printf("\nId: %d", s.id);
-	printf("\nNume: %s", s.nume);
-	printf("\nAn: %d\n", s.an);
-}
-
-Node* inserareStudentInArbore(Node** rad, Student s) {
-	if ((*rad)!=NULL) {
-		if (s.id < (*rad)->info.id) {
-			inserareStudentInArbore(&((*rad)->st), s);
-		}
-		else if(s.id>(*rad)->info.id){
-			inserareStudentInArbore(&((*rad)->dr),s);
-		}
-		else {
-			if ((*rad)->info.nume != NULL) {
-				free((*rad)->info.nume);
-			}
-			(*rad)->info.id = s.id;
-			(*rad)->info.nume = (char*)malloc(strlen(s.nume) + 1);
-			strcpy((*rad)->info.nume, s.nume);
-			(*rad)->info.an = s.an;
-		}
-	}
-	else {
-		(*rad) = malloc(sizeof(Node));
-		(*rad)->info = s;
-		(*rad)->info.nume = (char*)malloc(strlen(s.nume) + 1);
-		strcpy((*rad)->info.nume, s.nume);
-		(*rad)->st = NULL;
-		(*rad)->dr = NULL;
-	}
-}
-
-Node* citireArboreStudentiDinFisier(const char* numeFisier) {
-	FILE* file = fopen(numeFisier, "r");
-	Node* rad = NULL;
-	while (!feof(file)) {
-		Student s = citireStudentDinFisier(file);
-		inserareStudentInArbore(&rad, s);
-	}
-	fclose(file);
-	return rad;
-}
-
-//SRD - in ordine, radacina e in
-void afiseazaArboreInOrdine(Node* radacina) {
-	if (radacina!=NULL) {
-		afiseazaArboreInOrdine(radacina->st);
-		afiseazaStudent(radacina->info);
-		afiseazaArboreInOrdine(radacina->dr);
-	}
-}
-
-//RSD
-void afiseazaInPreordine(Node* radacina) {
-	if (radacina != NULL) {
-		afiseazaStudent(radacina->info);
-		afiseazaInPreordine(radacina->st);
-		afiseazaInPreordine(radacina->dr);
-	}
-}
-
-//SDR
-void afiseazaInPostordina(Node* radacina) {
-	if (radacina != NULL) {
-		afiseazaInPreordine(radacina->st);
-		afiseazaInPreordine(radacina->dr);
-		afiseazaStudent(radacina->info);
-	}
-}
-
-//cautare in arbore binar
-Student cautareStudentDupaId(Node* radacina, int idCautat) {
-	Student s;
-	s.id = -1;
-	s.nume = NULL; 
-	s.an = -1;
-	if (radacina) {
-		if (radacina->info.id == idCautat) {
-			s = radacina->info;
-			s.nume = (char*)malloc(strlen(radacina->info.nume) + 1);
-			strcpy(s.nume, radacina->info.nume);
-			
-			return s;
-		}
-		else if (radacina->info.id < idCautat) {
-			return cautareStudentDupaId(radacina->dr, idCautat);
-		}
-		else {
-			return cautareStudentDupaId(radacina->st, idCautat);
-		}
-	}
-	else {
-		return s;
-	}
-}
-
-void dezalocareArbore(Node** rad) {
-	if ((*rad)) {
-		dezalocareArbore(&((*rad)->st));
-		dezalocareArbore(&((*rad)->dr));
-		free((*rad)->info.nume);
-		free(*rad);
-		*rad = NULL;
-	}
-}
-
-int main() {
-	Student s1 = initializareStudent(4, "Ana I", 1);
-	afiseazaStudent(s1);
-	Node* arbore = citireArboreStudentiDinFisier("studenti.txt");
-	printf("-----------------InOrdine----------------\n");
-	afiseazaArboreInOrdine(arbore);
-	/*printf("-----------------PostOrdine----------------\n");
-	afiseazaInPostordina(arbore);
-	printf("-----------------PreOrdine----------------\n");
-	afiseazaInPreordine(arbore);*/
-	printf("\nCautare student dupa id: ");
-	Student studentGasit = cautareStudentDupaId(arbore, 2);
-	afiseazaStudent(studentGasit);
-	free(studentGasit.nume);
-	dezalocareArbore(&arbore);
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//struct Student {
+//	int id;
+//	char* nume;
+//	int an;
+//};
+//typedef struct Student Student;
+//typedef struct Node Node;
+//
+//struct Node {
+//	Student info;
+//	Node* st;
+//	Node* dr;
+//};
+//
+//Student initializareStudent(int id, const char* nume, int an) {
+//	Student s;
+//	s.id = id;
+//	s.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
+//	strcpy(s.nume, nume);
+//	s.an = an;
+//
+//	return s;
+//}
+//
+//Student citireStudentDinFisier(FILE* file) {
+//	char buffer[100];
+//	char sep[3] = ",\n";
+//	fgets(buffer, 100, file);
+//	char* aux;
+//	Student s;
+//	aux = strtok(buffer, sep);
+//	s.id = atoi(aux);
+//	aux = strtok(NULL, sep);
+//	s.nume = (char*)malloc(strlen(aux) + 1);
+//	strcpy(s.nume, aux);
+//	aux = strtok(NULL, sep);
+//	s.an = atoi(aux);
+//
+//	return s;
+//}
+//
+//void afiseazaStudent(Student s) {
+//	printf("\nId: %d", s.id);
+//	printf("\nNume: %s", s.nume);
+//	printf("\nAn: %d\n", s.an);
+//}
+//
+//Node* inserareStudentInArbore(Node** rad, Student s) {
+//	if ((*rad)!=NULL) {
+//		if (s.id < (*rad)->info.id) {
+//			inserareStudentInArbore(&((*rad)->st), s);
+//		}
+//		else if(s.id>(*rad)->info.id){
+//			inserareStudentInArbore(&((*rad)->dr),s);
+//		}
+//		else {
+//			if ((*rad)->info.nume != NULL) {
+//				free((*rad)->info.nume);
+//			}
+//			(*rad)->info.id = s.id;
+//			(*rad)->info.nume = (char*)malloc(strlen(s.nume) + 1);
+//			strcpy((*rad)->info.nume, s.nume);
+//			(*rad)->info.an = s.an;
+//		}
+//	}
+//	else {
+//		(*rad) = malloc(sizeof(Node));
+//		(*rad)->info = s;
+//		(*rad)->info.nume = (char*)malloc(strlen(s.nume) + 1);
+//		strcpy((*rad)->info.nume, s.nume);
+//		(*rad)->st = NULL;
+//		(*rad)->dr = NULL;
+//	}
+//}
+//
+//Node* citireArboreStudentiDinFisier(const char* numeFisier) {
+//	FILE* file = fopen(numeFisier, "r");
+//	Node* rad = NULL;
+//	while (!feof(file)) {
+//		Student s = citireStudentDinFisier(file);
+//		inserareStudentInArbore(&rad, s);
+//	}
+//	fclose(file);
+//	return rad;
+//}
+//
+////SRD - in ordine, radacina e in
+//void afiseazaArboreInOrdine(Node* radacina) {
+//	if (radacina!=NULL) {
+//		afiseazaArboreInOrdine(radacina->st);
+//		afiseazaStudent(radacina->info);
+//		afiseazaArboreInOrdine(radacina->dr);
+//	}
+//}
+//
+////RSD
+//void afiseazaInPreordine(Node* radacina) {
+//	if (radacina != NULL) {
+//		afiseazaStudent(radacina->info);
+//		afiseazaInPreordine(radacina->st);
+//		afiseazaInPreordine(radacina->dr);
+//	}
+//}
+//
+////SDR
+//void afiseazaInPostordina(Node* radacina) {
+//	if (radacina != NULL) {
+//		afiseazaInPreordine(radacina->st);
+//		afiseazaInPreordine(radacina->dr);
+//		afiseazaStudent(radacina->info);
+//	}
+//}
+//
+////cautare in arbore binar
+//Student cautareStudentDupaId(Node* radacina, int idCautat) {
+//	Student s;
+//	s.id = -1;
+//	s.nume = NULL; 
+//	s.an = -1;
+//	if (radacina) {
+//		if (radacina->info.id == idCautat) {
+//			s = radacina->info;
+//			s.nume = (char*)malloc(strlen(radacina->info.nume) + 1);
+//			strcpy(s.nume, radacina->info.nume);
+//			
+//			return s;
+//		}
+//		else if (radacina->info.id < idCautat) {
+//			return cautareStudentDupaId(radacina->dr, idCautat);
+//		}
+//		else {
+//			return cautareStudentDupaId(radacina->st, idCautat);
+//		}
+//	}
+//	else {
+//		return s;
+//	}
+//}
+//
+//void dezalocareArbore(Node** rad) {
+//	if ((*rad)) {
+//		dezalocareArbore(&((*rad)->st));
+//		dezalocareArbore(&((*rad)->dr));
+//		free((*rad)->info.nume);
+//		free(*rad);
+//		*rad = NULL;
+//	}
+//}
+//
+//
+//int main() {
+//	Student s1 = initializareStudent(4, "Ana I", 1);
+//	afiseazaStudent(s1);
+//	Node* arbore = citireArboreStudentiDinFisier("studenti.txt");
+//	printf("-----------------InOrdine----------------\n");
+//	afiseazaArboreInOrdine(arbore);
+//	/*printf("-----------------PostOrdine----------------\n");
+//	afiseazaInPostordina(arbore);
+//	printf("-----------------PreOrdine----------------\n");
+//	afiseazaInPreordine(arbore);*/
+//	printf("\nCautare student dupa id: ");
+//	Student studentGasit = cautareStudentDupaId(arbore, 2);
+//	afiseazaStudent(studentGasit);
+//	free(studentGasit.nume);
+//	dezalocareArbore(&arbore);
+//	return 0;
+//}
---
+++ seminarSDD/Task suplimentar AVL.c
@@ -0,0 +1,150 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//nu trebuie ca elementele din fisier sa fie sortate crescator
+//arborele binar de cautare echilibrat - vom avra nr aproape egal de nodruri
+//si in stanga si in dreapta
+//factorul de balans se calculeaza ca diferenta dintre nr de noduri 
+//al fiecarui sub arbore pot fii numai +1, -1 si 0, daca e diferit, avem 
+//dezechilibru
+
+struct Student {
+	int id;
+	char* nume;
+	int an;
+};
+
+typedef struct Student Student;
+typedef struct Node Node;
+
+struct Node {
+	Student info;
+	Node* st;
+	Node* dr;
+};
+
+Student citireStudentFisier(FILE* file) {
+	char buffer[100];
+	char sep[3] = ",\n";
+	fgets(buffer, 100, file);
+	Student s;
+	char* aux;
+	aux = strtok(buffer, sep);
+	s.id = atoi(aux);
+	aux = strtok(NULL, sep);
+	s.nume = (char*)malloc(strlen(aux) + 1);
+	strcpy(s.nume, aux);
+	aux = strtok(NULL, sep);
+	s.an = atoi(aux);
+	return s;
+}
+
+void afiseazaStudent(Student s) {
+	printf("Id: %d\n", s.id);
+	printf("Nume: %s\n", s.nume);
+	printf("An: %d\n\n", s.an);
+}
+
+int inaltimeArbore(Node* rad) {
+	if (rad) {
+		int inaltimeSt = inaltimeArbore(rad->st);
+		int inaltimeDr = inaltimeArbore(rad->dr);
+
+		return 1 + (inaltimeSt > inaltimeDr ? inaltimeSt : inaltimeDr);
+	}
+	return 0;
+
+}
+
+Node* rotireLaDreapta(Node* rad) {
+	if (rad) {
+		Node* aux = rad->st;
+		rad->st = aux->dr;
+		aux->dr = rad;
+		return aux;
+	}
+	return rad;
+}
+
+Node* rotireLaStanga(Node* rad) {
+	if (rad) {
+		Node* aux = rad->dr;
+		rad->dr = aux->st;
+		aux->st = rad;
+		return aux;
+	}
+	return rad;
+}
+
+int calculareGradEchilibru(Node* rad) {
+	if (rad) {
+		int inaltimeSt = inaltimeArbore(rad->st);
+		int inaltimeDr = inaltimeArbore(rad->dr);
+		return inaltimeSt - inaltimeDr;
+		
+	}
+	else {
+		return 0;
+	}
+}
+
+Node* inserareStudentInAVL(Node* rad,Student s) {
+	if (rad) {
+		if ( s.id < rad->info.id ) {
+			rad->st = inserareStudentInAVL(rad->st, s);
+		}
+		else {
+			rad->dr = inserareStudentInAVL(rad->dr, s);
+		}
+		if (calculareGradEchilibru(rad) == 2) {
+			if (calculareGradEchilibru(rad->st) != 1) {
+				rad->st = rotireLaStanga(rad->st);
+			}
+			rad = rotireLaDreapta(rad);
+		}
+		if (calculareGradEchilibru(rad) == -2) {
+			if (calculareGradEchilibru(rad->dr) != -1) {
+				rad->dr = rotireLaDreapta(rad->dr);
+			}
+			rad = rotireLaStanga(rad);
+		}
+
+		return rad;
+	}
+	else {
+		Node* nou = (Node*)malloc(sizeof(Node));
+		nou->dr = NULL;
+		nou->st = NULL;
+		nou->info = s;
+		return nou;
+	}
+}
+
+
+
+Node* citireArboreDinFisier(const char* numeFisier) {
+	Node* rad = NULL;
+	FILE* file = fopen(numeFisier, "r");
+	while (!feof(file)) {
+		rad = inserareStudentInAVL(rad, citireStudentFisier(file));
+	}
+	fclose(file);
+	return rad;
+}
+
+void afisareArboreInPreordine(Node* rad) {
+	if (rad) {
+		afiseazaStudent(rad->info);
+		afisareArboreInPreordine(rad->st);
+		afisareArboreInPreordine(rad->dr);
+	}
+}
+
+
+int main() {
+	Node* rad = citireArboreDinFisier("studenti.txt");
+	afisareArboreInPreordine(rad);
+	return 0;
+}
